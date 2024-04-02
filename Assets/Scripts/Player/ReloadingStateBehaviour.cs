98d5bf1ef7d6d49616a89776c3b09f48d3ffb65d
Demionchek <Demionchek@gmail.com>
2024-04-02 09:52:47 +0300

    fix for double reloading on remote player

--- Assets/Scripts/Player/ReloadingStateBehaviour.cs
+++ Assets/Scripts/Player/ReloadingStateBehaviour.cs
@@ -4,6 +4,8 @@ public class ReloadingStateBehaviour : StateMachineBehaviour {
 
     private PlayerScript playerScript;
 
+    private static int BODY_LAYER = 1;
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         if (playerScript == null) {
              playerScript = animator.transform.parent.GetComponent<PlayerScript>();
@@ -15,11 +17,18 @@ public class ReloadingStateBehaviour : StateMachineBehaviour {
         }
     }
 
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (playerScript != null) {
+            if (stateInfo.normalizedTime > 0.5f && stateInfo.normalizedTime < 1) {
+                if (playerScript.networkAnimator != null)
+                    playerScript.networkAnimator.animator.SetBool("Reload", false);
+            }
+        }
+    }
+
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         if (playerScript != null) {
             playerScript.isReloading = false;
-            if (playerScript.networkAnimator != null)
-            playerScript.networkAnimator.animator.SetBool("Reload", false);
         }
     }
 }
